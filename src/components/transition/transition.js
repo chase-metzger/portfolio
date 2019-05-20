9a5080b4abaedeee8d88679e77bf16cb3e5c692d
Conrad Chase Metzger <cmetzger@bearcubs.santarosa.edu>
2019-05-20 15:54:34 -0700

    Move the RoutesContainer out of the render function in the Transition component

--- src/components/transition/transition.js
+++ src/components/transition/transition.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import posed, { PoseGroup } from 'react-pose';
 import { timeout } from 'constants/transition';
 
+const RoutesContainer = posed.div({
+  enter: { opacity: 1, delay: timeout, delayChildren: timeout },
+  exit: { opacity: 0 },
+});
+
 class Transition extends PureComponent {
   render() {
     const { children, location } = this.props;
-
-    const RoutesContainer = posed.div({
-      enter: { opacity: 1, delay: timeout, delayChildren: timeout },
-      exit: { opacity: 0 },
-    });
 
     // To enable page transitions on mount / initial load,
     // use the prop `animateOnMount={true}` on `PoseGroup`.
